d71ec589a0e2d85b5b1be635755c9147074b0193
qamichaelmaia <contatomichaelmaia@gmail.com>
2024-03-14 13:42:32 -0300

    ajustes

--- Mod21_Teste_de_Unidade/tarefa/codigo.js
+++ Mod21_Teste_de_Unidade/tarefa/codigo.js
@@ -1,5 +1,6 @@
 const soma = (a, b) => a + b
 const dobro = valor => valor * 2
 const divisor = valor => valor / 2
+const sub = (a, b) => a - b
 
-module.exports = {soma, dobro,divisor}
+module.exports = {soma, dobro,divisor,sub}
--- Mod21_Teste_de_Unidade/tarefa/codigo.test.js
+++ Mod21_Teste_de_Unidade/tarefa/codigo.test.js
@@ -1,4 +1,4 @@
-const { soma, dobro, divisor } = require("./codigo");
+const { soma, sub, dobro, divisor } = require("./codigo");
 
 describe("funções matemáticas", () => {
     beforeAll(() => {
@@ -21,13 +21,23 @@ describe("funções matemáticas", () => {
         expect(soma(2, 5)).toBe(7);
         expect(soma(2, 4)).toBe(6);
         expect(soma(21, 44)).toBe(65);
+
     });
+
+    it("subtração de dois valores", () => {
+        expect(sub(8, 5)).toBe(3);
+        expect(sub(57, 4)).toBe(53);
+        expect(sub(84, 44)).toBe(40);
+    });
+
     it("dobro de um valor", () => {
         expect(dobro(4)).toBe(8);
         expect(dobro(35)).toBe(70);
     });
+
     it("divisão de um valor", () => {
         expect(divisor(10)).toBe(5);
         expect(divisor(300)).toBe(150);
     });
+
 });
