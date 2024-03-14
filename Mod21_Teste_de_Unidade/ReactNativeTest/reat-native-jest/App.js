fa38b82f99c8684b8ba76e8579aa2ae4ff50014b
qamichaelmaia <contatomichaelmaia@gmail.com>
2024-03-13 21:03:34 -0300

    teste

--- Mod21_Teste_de_Unidade/ReactNativeTest/reat-native-jest/App.js
+++ Mod21_Teste_de_Unidade/ReactNativeTest/reat-native-jest/App.js
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Meu primeiro App React Native na EBAC</Text>
+      <Text>Meu primeiro App React Native na EBAC </Text>
     </View>
   );
 }
